c2828e86157cce7232d7bf0b6cbd161e3c5816bc
Walocial <Walocial@hotmail.com>
2025-10-27 08:13:30 +0100

    v1.0.5.1 - Fleshing out Contact Page
    - Created a more personalized image for the contact page
    - Added text to the Contact page.
    - Corrected contact information.
    - Added functionality to the "Cases" button by having it direct the user to my prototype portfolio in PDF format. This is intended to be a temporary solution while I work on a proper Cases-page.

--- contact.php
+++ contact.php
@@ -7,15 +7,23 @@
         <section id="contactSection" class="fullScreenSection">
             <ul id="infoCard-Container" tabindex="-1">
                 <li class="infoCard">
-                    <img class="cardImage" src="assets/media/TobiasCoding.webp" alt="">
+                    <img class="cardImage" src="assets/media/TobiasKontakt_Cropped.webp" alt="">
                     <div class="cardInfo">
                         <h1 id="contactH1">Kontakt Mig</h1>    
                         <!-- <h2 class="cardTitle">Personlighed & Fritid</h2> -->
                         <div class="horizontalDivider"></div>
+                    <p>Passer jeg ind i jeres virksomhed?</p>
+                    <p>Skab kontaken og lad os finde ud af det sammen!</p>
+                    <p>Jeg kan kontaktes følgende måder:</p>
                     <nav id="socials_grid">
                         <a href="https://www.linkedin.com/in/tobias-juul-michaelsen-8b1769252/" id="social_linkedin" class="social_button" tabindex="0" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        <a href="tel:+4541816360" id="social_phone" class="social_button" tabindex="0"><i class="fas fa-phone"></i></a>
                         <a href="mailto:tobias@chakemato.dk" id="social_mail" class="social_button" tabindex="0"><i class="fas fa-envelope"></i></a>
                     </nav>
+                    <span>
+                        <i class="fas fa-warning"></i>
+                        OBS: Jeg sætter pris på telefonisk samtale efter aftale over mail, SMS, eller LinkedIn!
+                    </span>
                 </li>
             </ul>
         </section>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <nav id="floating_nav_wrapper">
                 <ul class="ctaNavigation">
                     <li id="cta_about" class="left_btn link_list_item" data-hovertext="Her kan du lære mig at kende" aria-label="Læs mere om mig"><a href="about.php" data-hovertext="Lær mig at kende">Om mig</a></li>
-                    <li id="cta_cases" class="left_btn link_list_item" data-hovertext="Se mine tidligere projekter" aria-label="Se mine cases"><a href="cases.php">Cases</a></li>
+                    <li id="cta_cases" class="left_btn link_list_item" data-hovertext="Se mine tidligere projekter" aria-label="Se mine cases"><a href="assets/downloads/TobiasJuulMichaelsen_Portefølje_2025.pdf" target="_blank">Cases</a></li>
                     <li id="cta_CV" class="right_btn link_list_item" data-hovertext="Download en pdf af mit CV" aria-label="Download mit CV"><a href="assets/downloads/TobiasJuulMichaelsenCV-2025.pdf" download>Hent CV<i class="fas fa-download"></i></a></li>
                     <li id="cta_contact" class="right_btn link_list_item" data-hovertext="Klik for at skabe kontakten" aria-label="Kontakt mig"><a href="contact.php">Kontakt mig</a></li>
                 </ul>
@@ -20,7 +20,7 @@
         <section id="navContent">
             <nav>
                 <ul class="ctaNavigation">
-                    <li class="link_list_item"><a href="cases.php">Cases</a></li>
+                    <li class="link_list_item"><a href="assets/downloads/TobiasJuulMichaelsen_Portefølje_2025.pdf">Cases</a></li>
                     <li class="link_list_item"><a href="about.php">Om mig</a></li>
                     <li class="link_list_item"><a href="assets/downloads/TobiasJuulMichaelsenCV-2024.pdf" download>Hent CV<i class="fas fa-download"></i></a></li>
                     <li class="link_list_item"><a href="contact.php">Kontakt mig</a></li>
